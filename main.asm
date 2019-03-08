8b60039a6bedb0942b8cc2e1650aada62fbeb2be
Ammar Faizi <ammarfaizi2@gmail.com>
2019-03-08 14:47:56 +0700

    Fix found data

--- main.asm
+++ main.asm
@@ -1,12 +1,19 @@
-
+;
+; @author Ammar Faizi <ammarfaizi2@gmail.com> https://ww.facebook.com/ammarfaizi2
+;
 section .data
 	a1_s db "Masukkan ID barang: "
 	a1_l equ $-a1_s
+	a2_s db "Sedang melakukan pencarian...",10
+	a2_l equ $-a2_s
+	a3_s db 10,"Barang ditemukan!",10
+	a3_l equ $-a3_s
 	filename db "id_barang.txt"
 
 section .bss
 	input resb 5
-	search_buf resb 4
+	search_buf resb 5
+	fetch_buf resb 1
 
 section .text
 	global _start
@@ -16,13 +23,17 @@ _start:
 	mov rdx,a1_l
 	call mp
 	call get_id
-	mov rsi,input
-	mov rdx,rax
-	call mp
+	call show_wait
 	call open_file
 	mov r9,rax
 	call search
 	call exit
+
+show_wait:
+	mov rsi,a2_s
+	mov rdx,a2_l
+	call mp
+	ret
 
 open_file:
 	mov rax,2
@@ -32,10 +43,51 @@ open_file:
 	ret
 
 search:
+	cmp r10,1
+	je chread
 	call sread
 	call check
-	cmp r10,1
-	jne search
+	cmp rax,1
+	je bef_found
+	jmp search
+	ret
+
+bef_found:
+	mov rsi,a3_s
+	mov rdx,a3_l
+	call mp
+	call found
+	ret
+
+found:
+	mov rax,0
+	mov rdi,r9
+	mov rsi,fetch_buf
+	mov rdx,1
+	syscall
+	cmp rax,0
+	je exit
+	mov rsi,fetch_buf
+	mov rdx,rax
+	call mp
+	mov rdx,[fetch_buf]
+	cmp rdx,10
+	jne found
+	ret
+
+check:
+	mov rdi,[search_buf]
+	mov rsi,[input]
+	and rsi,0x00000000ffffff
+	and rdi,0x00ffffff
+	cmp rsi,rdi
+	je found_flag
+	ret
+
+found_flag:
+	mov rax,1
+	mov r10,1
+	jmp check+35
 	ret
 
 chread:
@@ -44,6 +96,8 @@ chread:
 	mov rsi,search_buf
 	mov rdx,1
 	syscall
+	mov r10,0
+	call search
 	ret
 
 sread:
